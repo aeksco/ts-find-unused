eb675f9b71c310260289859ff2d57bd7851cd0e4
Alexander Schwartzberg <aeksco@gmail.com>
2021-10-30 12:10:00 -0400

    Annotate printToStdout

--- src/printToStdout.ts
+++ src/printToStdout.ts
@@ -1,3 +1,8 @@
+/**
+ * printToStdout
+ * Helper function to print each line to stdout
+ * @param lines - array of lines being printed to stdout
+ */
 export function printToStdout(lines: string[]): void {
   console.log("\n");
   lines.forEach(line => {
